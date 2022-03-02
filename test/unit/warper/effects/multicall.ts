10b1970aaf7a2fbdcbb38cffc67751174edb9750
Roberts Ivanovs <roberts.ivanovs@blockvis.com>
2022-03-02 17:55:50 +0200

    test: naming change

--- test/unit/warper/effects/multicall.ts
+++ test/unit/warper/effects/multicall.ts
@@ -6,13 +6,13 @@ import { AddressZero } from '../../../shared/types';
 export function shouldBehaveLikeMulticall(): void {
   describe('Multicall methods', function () {
     let warper: Warper;
-    let underlying: ERC721Mock;
+    let originalAsset: ERC721Mock;
 
     let tokenOwner: SignerWithAddress;
 
     beforeEach(function () {
       warper = this.contracts.presets.agnosticWarper;
-      underlying = this.mocks.assets.erc721;
+      originalAsset = this.mocks.assets.erc721;
 
       [tokenOwner] = this.signers.unnamed;
     });
@@ -21,17 +21,17 @@ export function shouldBehaveLikeMulticall(): void {
       context('Call the mint method multiple times', () => {
         it('mints n tokens', async () => {
           // Construct the call
-          const call1 = underlying.interface.encodeFunctionData('mint', [tokenOwner.address, 1]);
-          const call2 = underlying.interface.encodeFunctionData('mint', [tokenOwner.address, 22]);
-          const call3 = underlying.interface.encodeFunctionData('mint', [tokenOwner.address, 42]);
+          const call1 = originalAsset.interface.encodeFunctionData('mint', [tokenOwner.address, 1]);
+          const call2 = originalAsset.interface.encodeFunctionData('mint', [tokenOwner.address, 22]);
+          const call3 = originalAsset.interface.encodeFunctionData('mint', [tokenOwner.address, 42]);
 
           // Execute the call
           const tx = await warper.multicall([call1, call2, call3]);
 
           // Assert the call
-          await expect(tx).to.emit(underlying, 'Transfer').withArgs(AddressZero, tokenOwner.address, 1);
-          await expect(tx).to.emit(underlying, 'Transfer').withArgs(AddressZero, tokenOwner.address, 22);
-          await expect(tx).to.emit(underlying, 'Transfer').withArgs(AddressZero, tokenOwner.address, 42);
+          await expect(tx).to.emit(originalAsset, 'Transfer').withArgs(AddressZero, tokenOwner.address, 1);
+          await expect(tx).to.emit(originalAsset, 'Transfer').withArgs(AddressZero, tokenOwner.address, 22);
+          await expect(tx).to.emit(originalAsset, 'Transfer').withArgs(AddressZero, tokenOwner.address, 42);
         });
       });
     });
