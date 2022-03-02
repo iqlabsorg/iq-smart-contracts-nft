28d4176d00e183d214e69f895bb82057bde0b4cc
Roberts Ivanovs <roberts.ivanovs@blockvis.com>
2022-03-02 16:18:01 +0200

    feat(erc721-warper-configurable): added data validation for rental and availability periods

--- contracts/warper/ERC721/extensions/ERC721WarperConfigurable.sol
+++ contracts/warper/ERC721/extensions/ERC721WarperConfigurable.sol
@@ -5,9 +5,26 @@ import "../ERC721Warper.sol";
 import "../../utils/RentalParamStore.sol";
 import "../../IRentalParamStore.sol";
 
-//todo: add Multicall trait
-//todo: validate: minRentalPeriod <= maxRentalPeriod (revert with InvalidMinRentalPeriod / InvalidMaxRentalPeriod)
-//todo: validate: availabilityPeriodStart < availabilityPeriodEnd (revert with InvalidAvailabilityPeriodStart / InvalidAvailabilityPeriodEnd)
+/**
+ * @dev Thrown when the the min rental period is not strictly lesser than max rental period
+ */
+error InvalidMinRentalPeriod();
+
+/**
+ * @dev Thrown when the max rental period is not greater or equal than min rental period
+ */
+error InvalidMaxRentalPeriod();
+
+/**
+ * @dev Thrown when the availability period start time is not strictly lesser than the end time
+ */
+error InvalidAvailabilityPeriodStart();
+
+/**
+ * @dev Thrown when the availability period end time is not greater or equal than the start time
+ */
+error InvalidAvailabilityPeriodEnd();
+
 abstract contract ERC721WarperConfigurable is ERC721Warper, RentalParamStore, IRentalParamStore {
     function _ERC721WarperConfigurable_init() internal onlyInitializing {
         // Store default values.
@@ -29,6 +46,7 @@ abstract contract ERC721WarperConfigurable is ERC721Warper, RentalParamStore, IR
      * @inheritdoc IRentalParamStore
      */
     function __setMinRentalPeriod(uint32 minRentalPeriod) external virtual onlyWarperAdmin {
+        if (minRentalPeriod > _maxRentalPeriod()) revert InvalidMinRentalPeriod();
         _setMinRentalPeriod(minRentalPeriod);
     }
 
@@ -36,6 +54,7 @@ abstract contract ERC721WarperConfigurable is ERC721Warper, RentalParamStore, IR
      * @inheritdoc IRentalParamStore
      */
     function __setMaxRentalPeriod(uint32 maxRentalPeriod) external virtual onlyWarperAdmin {
+        if (_minRentalPeriod() > maxRentalPeriod) revert InvalidMaxRentalPeriod();
         _setMaxRentalPeriod(maxRentalPeriod);
     }
 
@@ -43,6 +62,7 @@ abstract contract ERC721WarperConfigurable is ERC721Warper, RentalParamStore, IR
      * @inheritdoc IRentalParamStore
      */
     function __setAvailabilityPeriodStart(uint32 availabilityPeriodStart) external virtual onlyWarperAdmin {
+        if (availabilityPeriodStart >= _availabilityPeriodEnd()) revert InvalidAvailabilityPeriodStart();
         _setAvailabilityPeriodStart(availabilityPeriodStart);
     }
 
@@ -50,6 +70,7 @@ abstract contract ERC721WarperConfigurable is ERC721Warper, RentalParamStore, IR
      * @inheritdoc IRentalParamStore
      */
     function __setAvailabilityPeriodEnd(uint32 availabilityPeriodEnd) external virtual onlyWarperAdmin {
+        if (_availabilityPeriodStart() >= availabilityPeriodEnd) revert InvalidAvailabilityPeriodEnd();
         _setAvailabilityPeriodEnd(availabilityPeriodEnd);
     }
 
