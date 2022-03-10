863cea7bc9dea08ad186de06f896ebc7f5e621c4
Vsevolods Mihailovs <cheelahim@gmail.com>
2022-03-10 13:25:16 +0200

    feat: renting lib & controller interface

---
+++ contracts/renting/Renting.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+library Renting {
+    //todo: docs
+    struct Params {
+        uint256 listingId;
+        address warper;
+        address renter;
+        uint32 period;
+    }
+}
