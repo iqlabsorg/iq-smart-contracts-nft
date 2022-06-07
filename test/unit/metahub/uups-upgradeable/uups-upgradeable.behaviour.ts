77fd49617fcae0d0b19adc782cd9b4c9508c8500
Roberts Ivanovs <roberts.ivanovs@blockvis.com>
2022-06-07 15:01:19 +0300

    test: metahub upgrade tests

--- test/unit/metahub/uups-upgradeable/uups-upgradeable.behaviour.ts
+++ test/unit/metahub/uups-upgradeable/uups-upgradeable.behaviour.ts
@@ -1,6 +1,6 @@
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { expect } from 'chai';
-import { upgrades } from 'hardhat';
+import hre, { upgrades } from 'hardhat';
 import {
   Accounts__factory,
   Assets__factory,
@@ -9,12 +9,12 @@ import {
   MetahubV2Mock__factory,
   Rentings__factory,
   UUPSUpgradeable,
+  UUPSUpgradeable__factory,
   Warpers__factory,
 } from '../../../../typechain';
 import { MetahubLibraryAddresses } from '../../../../typechain/factories/contracts/metahub/Metahub__factory';
+import { ADDRESS_ZERO } from '../../../shared/types';
 import { AccessControlledHelper } from '../../../shared/utils';
-
-// TODO make this generic so it can be re-used for multiple contracts
 
 export function shouldBehaveLikeUUPSUpgradeable(): void {
   describe('UUPS Upgradeable', function () {
@@ -24,34 +24,81 @@ export function shouldBehaveLikeUUPSUpgradeable(): void {
     let deployer: SignerWithAddress;
 
     beforeEach(async function () {
-      upgradeable = this.contracts.uupsUpgradeable;
-
       deployer = this.signers.named.deployer;
 
-      // Deploy external libraries used by Metahub.
-      const rentingsLib = await new Rentings__factory(deployer).deploy();
-      const listingsLib = await new Listings__factory(deployer).deploy();
-      const assetsLib = await new Assets__factory(deployer).deploy();
-      const warpersLib = await new Warpers__factory(deployer).deploy();
-      const accountsLib = await new Accounts__factory(deployer).deploy();
+      upgradeable = UUPSUpgradeable__factory.connect(this.contracts.uupsUpgradeable.address, deployer);
 
-      const metahubLibs: MetahubLibraryAddresses = {
-        'contracts/renting/Rentings.sol:Rentings': rentingsLib.address,
-        'contracts/listing/Listings.sol:Listings': listingsLib.address,
-        'contracts/asset/Assets.sol:Assets': assetsLib.address,
-        'contracts/warper/Warpers.sol:Warpers': warpersLib.address,
-        'contracts/accounting/Accounts.sol:Accounts': accountsLib.address,
-      };
+      // Deploy external libraries used by Metahub.
+      const metahubLibs = (await hre.run('deploy:metahub-libraries')) as MetahubLibraryAddresses;
 
       metahubV2Factory = new MetahubV2Mock__factory(metahubLibs, deployer);
     });
 
-    describe('upgradeTo', () => {
+    context('upgradeTo', () => {
+      let implInstance: MetahubV2Mock;
+      beforeEach(async () => {
+        implInstance = await metahubV2Factory.deploy();
+      });
+
+      AccessControlledHelper.onlyAdminCan(async signer => {
+        await upgradeable.connect(signer).upgradeTo(implInstance.address);
+
+        const metahubV2 = metahubV2Factory.attach(upgradeable.address);
+        await expect(metahubV2.version()).to.eventually.eq('V2');
+      });
+    });
+
+    context('hre.upgrades.upgradeProxy', () => {
       AccessControlledHelper.onlyAdminCan(async signer => {
         const metahubV2 = (await upgrades.upgradeProxy(upgradeable, metahubV2Factory.connect(signer), {
           unsafeAllow: ['delegatecall', 'external-library-linking'],
         })) as MetahubV2Mock;
         expect(metahubV2.address).to.eq(upgradeable.address);
+      });
+    });
+
+    context('using prepared tasks', () => {
+      it('upgrade:metahub', async function () {
+        // NOTE: we're deploying a new instance of MetahubV2Mock and then upgrading
+        // it to Metahub contract because the upgrade task only uses the Metahub
+        // contract as the new implementation!
+
+        // Deploy the MetahubV2Mock
+        const upgradeable = (await upgrades.deployProxy(metahubV2Factory, [], {
+          initializer: false,
+          unsafeAllow: ['delegatecall', 'external-library-linking'],
+          kind: 'uups',
+        })) as MetahubV2Mock;
+
+        await upgradeable.initialize({
+          warperPresetFactory: ADDRESS_ZERO,
+          universeRegistry: ADDRESS_ZERO,
+          listingStrategyRegistry: ADDRESS_ZERO,
+          assetClassRegistry: ADDRESS_ZERO,
+          acl: this.contracts.acl.address,
+          baseToken: ADDRESS_ZERO,
+          rentalFeePercent: 100,
+        });
+
+        const metahubV2 = (await hre.run('upgrade:metahub', { proxy: upgradeable.address })) as MetahubV2Mock;
+
+        // NOTE: if the lambda function is async then the assertion does not work :)
+        // eslint-disable-next-line @typescript-eslint/promise-function-async
+        expect(() => metahubV2.version()).to.throw(TypeError); // Method does not exist!
+      });
+
+      it('upgrade:unsafe:proxy', async () => {
+        // Deploy impl
+        const implInstance = await metahubV2Factory.deploy();
+
+        // Upgrade
+        await hre.run('upgrade:unsafe:proxy', {
+          proxy: upgradeable.address,
+          implementation: implInstance.address,
+        });
+        const metahubV2 = metahubV2Factory.attach(upgradeable.address);
+
+        // Assert
         await expect(metahubV2.version()).to.eventually.eq('V2');
       });
     });
