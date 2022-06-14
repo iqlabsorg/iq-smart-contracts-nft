741b2faa8c6ae87fe3f264422824cf2a6a24720a
Vsevolods Mihailovs <cheelahim@gmail.com>
2022-06-14 19:02:44 +0300

    chore: remove unused network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -58,10 +58,6 @@ const config: HardhatUserConfig = {
     supervisor: 6,
   },
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL ?? '',
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-    },
     bscTestnet: {
       url: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
       accounts,
