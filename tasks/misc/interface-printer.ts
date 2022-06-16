cfe56c9a62ca793a3173a23873f152b025361f24
Vsevolods Mihailovs <cheelahim@gmail.com>
2022-06-16 13:06:23 +0300

    refactor: modify interface printer contract and corresponding task

--- tasks/misc/interface-printer.ts
+++ tasks/misc/interface-printer.ts
@@ -1,9 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-call */
 import { task } from 'hardhat/config';
-import { InterfacePrinter__factory } from '../../typechain/factories/contracts/mocks/InterfacePrinter__factory';
+import { InterfacePrinter__factory } from '../../typechain';
 
 task('misc:print-interfaces', 'Print interfaces IDs to the stdout').setAction(async (_args, hre) => {
   const deployer = await hre.ethers.getNamedSigner('deployer');
-
   const printer = await new InterfacePrinter__factory(deployer).deploy();
-  console.log('universeToken interfaceId: ', await printer.universeToken());
+  const interfaces = await printer.interfaces();
+  console.table(interfaces, ['name', 'id']);
 });
