7be6ebc5408e003981097a736502cb84f031587e
Roberts Ivanovs <roberts.ivanovs@blockvis.com>
2022-06-21 18:49:03 +0300

    refactor: preset configurable `__initialize` params changed to `memory`

--- contracts/warper/ERC721/presets/ERC721PresetConfigurable.sol
+++ contracts/warper/ERC721/presets/ERC721PresetConfigurable.sol
@@ -16,7 +16,7 @@ contract ERC721PresetConfigurable is
     /**
      * @inheritdoc IWarperPreset
      */
-    function __initialize(bytes calldata config) public virtual warperInitializer {
+    function __initialize(bytes memory config) public virtual warperInitializer {
         // Decode config
         (address original, address metahub) = abi.decode(config, (address, address));
         _Warper_init(original, metahub);
