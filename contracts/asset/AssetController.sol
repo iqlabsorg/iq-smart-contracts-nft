2e64de0a78ae0da4338271f555c9f3c1ca95d1f3
Vsevolods Mihailovs <cheelahim@gmail.com>
2022-06-21 17:55:49 +0300

    feat: introduce asset controller fallback function

--- contracts/asset/AssetController.sol
+++ contracts/asset/AssetController.sol
@@ -6,6 +6,16 @@ import "./IAssetController.sol";
 
 abstract contract AssetController is IAssetController, ERC165 {
     /**
+     * The fallback function is needed to ensure forward compatibility with Metahub.
+     * When introducing a new version of controller with additional external functions,
+     * it must be safe to call the those new functions on previous generation of controllers and it must not cause
+     * the transaction revert.
+     */
+    fallback() external {
+        // solhint-disable-previous-line no-empty-blocks, payable-fallback
+    }
+
+    /**
      * @inheritdoc IERC165
      */
     function supportsInterface(bytes4 interfaceId) public view virtual override(ERC165, IERC165) returns (bool) {
