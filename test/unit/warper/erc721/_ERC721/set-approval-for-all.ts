b2d19a35ffe2218b8d173d8a9069565725bbacde
Roberts Ivanovs <roberts.ivanovs@blockvis.com>
2022-07-04 16:57:58 +0300

    feat: erc721 warpers `getApproved` and `isApprovedForAll` will return metahub

--- test/unit/warper/erc721/_ERC721/set-approval-for-all.ts
+++ test/unit/warper/erc721/_ERC721/set-approval-for-all.ts
@@ -7,19 +7,30 @@ export function shouldBehaveLikeSetApprovalForAl(): void {
     let erc721warper: IERC721Warper;
     let assetOwner: SignerWithAddress;
     let operator: SignerWithAddress;
+    let stranger: SignerWithAddress;
 
     beforeEach(function () {
       erc721warper = this.contracts.erc721Warper;
 
       assetOwner = this.signers.named.assetOwner;
       operator = this.signers.named.operator;
+      [stranger] = this.signers.unnamed;
     });
 
     context('When calling `isApprovedForAll`', () => {
-      it('reverts', async () => {
-        await expect(
-          erc721warper.connect(assetOwner).isApprovedForAll(assetOwner.address, operator.address),
-        ).to.be.revertedWith('MethodNotAllowed()');
+      context('passing the Metahub address as the operator', () => {
+        it('returns true', async () => {
+          await expect(
+            erc721warper.connect(assetOwner).isApprovedForAll(assetOwner.address, await erc721warper.__metahub()),
+          ).to.eventually.be.true;
+        });
+      });
+
+      context('passing a strangers address as the operator', () => {
+        it('returns false', async () => {
+          await expect(erc721warper.connect(assetOwner).isApprovedForAll(assetOwner.address, stranger.address)).to
+            .eventually.be.false;
+        });
       });
     });
 
